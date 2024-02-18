2945e50ef9761f3591fb7de13accef594fc741f9
Liam O'Dell <liam.riley.odell@gmail.com>
2024-02-17 21:39:08 -0700

    fixed mobile visual bug

--- src/app/lib/components/GameOverModal.tsx
+++ src/app/lib/components/GameOverModal.tsx
@@ -9,7 +9,7 @@ export default function GameOverModal({ modalIsOpen, score, onClick }: GameOverM
       isOpen={modalIsOpen}
       className="w-1/4 max-h-fit bg-slate-50 text-center fixed top-1/2 left-1/2 border-black border-2 rounded-lg p-4 -translate-y-1/2 -translate-x-1/2 outline-none"
     >
-      <p className="text-xl font-bold">
+      <p className="text-xl font-bold text-black">
         Game Over
         <br />
         <br />
